1bdbfd45c217122c1a2be7a76f8d57cc4c200506
Rob M <robert.myers@incling.co.uk>
2019-12-10 17:53:29 +0000

    updated urls to support Django 2

--- rest_framework_tus/urls.py
+++ rest_framework_tus/urls.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
+import django
 from django.conf.urls import url, include
 from rest_framework_tus.views import UploadViewSet
 
 from .routers import TusAPIRouter
 
+app_name = 'rest_framework_tus'
+
 router = TusAPIRouter()
 router.register(r'files', UploadViewSet, base_name='upload')
 
-urlpatterns = [
-    url(r'', include(router.urls, namespace='api'))
-]
+if django.VERSION >= (2, 0):
+    urlpatterns = [
+        url(r'', include((router.urls, 'rest_framework_tus'), namespace='api'))
+    ]
+else:
+    urlpatterns = [
+        url(r'', include(router.urls, namespace='api'))
+    ]
